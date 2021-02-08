029e0eb28e3e112204d143a5fb7a39a85684138c
ipozdeev <igor.pozdeev@unisg.ch>
2021-02-08 22:56:48 +0100

    INITIAL COMMIT project structure, scrapers, tests

---
+++ scraper.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+
+
+class ConcertScraper:
+
+    def __init__(self):
+        pass
+
+    def get_event(self, url: str) -> dict:
+        """
+
+        Parameters
+        ----------
+        url : str
+
+        Returns
+        -------
+        dict
+
+        """
+        pass
+
+
+class WigmoreHallScraper(ConcertScraper):
+
+    def get_event(self, url: str) -> dict:
+
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ tests/test_scraper.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class TestWigmoreHallScraper(TestCase):
+    def test_get_event(self):
+        url = "https://wigmore-hall.org.uk/whats-on/the-sixteen-202102151930"
+        self.fail()
