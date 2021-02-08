ac3a64acc5858a75f754e1d05ac5f5f1e517fa88
ipozdeev <igor.pozdeev@unisg.ch>
2021-02-08 23:50:37 +0100

    PCMSConcerts add scraper and test; test passed

--- scraper.py
+++ scraper.py
@@ -1,10 +1,13 @@
 from bs4 import BeautifulSoup
+import requests
+import datetime
+import pytz
 
 
 class ConcertScraper:
 
-    def __init__(self):
-        pass
+    def __init__(self, tz):
+        self.tz = tz
 
     def get_event(self, url: str) -> dict:
         """
@@ -20,12 +23,49 @@ class ConcertScraper:
         """
         pass
 
+    # TODO: decorators for event duration
+
 
 class WigmoreHallScraper(ConcertScraper):
 
     def get_event(self, url: str) -> dict:
 
+        #TODO: use selenium here
+
         pass
+
+
+class PSCMScraper(ConcertScraper):
+    def __init__(self):
+        super(PSCMScraper, self).__init__(pytz.timezone("America/New_York"))
+
+    def get_event(self, url: str) -> dict:
+
+        # parse, create soup
+        # TODO: relocate to parent
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        # info, in the title of the page
+        summ = soup.title.text
+
+        # start date
+        evt_dt = soup.find("span", itemprop="startDate")
+        evt_dt = datetime.datetime.strptime(evt_dt.text,
+                                            "%A, %B %d, %Y - %I:%M %p")
+        evt_dt = self.tz.localize(evt_dt)
+
+        # return
+        res = {
+            "start": {
+                "dateTime": evt_dt.isoformat(),
+                "timeZone": self.tz.zone
+            },
+            'summary': summ,
+            'description': url,
+        }
+
+        return res
 
 
 if __name__ == '__main__':
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,7 +1,47 @@
-from unittest import TestCase
+import unittest
+import datetime
+import pytz
+
+from ..scraper import (WigmoreHallScraper, PSCMScraper)
 
 
-class TestWigmoreHallScraper(TestCase):
+@unittest.skip
+class TestWigmoreHallScraper(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.scraper = WigmoreHallScraper()
+        self.tz = pytz.timezone("Europe/London")
+        self.startTime = \
+            self.tz.localize(datetime.datetime(2021, 2, 15, 19, 30))
+        self.summary = "Anon, Browne, Cornysh and more by The Sixteen"
+
     def test_get_event(self):
         url = "https://wigmore-hall.org.uk/whats-on/the-sixteen-202102151930"
-        self.fail()
+        res_event = self.scraper.get_event(url)
+
+        self.assertEqual(res_event["summary"], self.summary)
+        self.assertEqual(res_event["start"], dict(dateTime=self.startTime,
+                                                  timeZone=self.tz.zone))
+
+
+class TestPSCMScraper(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.scraper = PSCMScraper()
+        self.tz = pytz.timezone("America/New_York")
+        self.startTime = \
+            self.tz.localize(datetime.datetime(2021, 2, 10, 18))
+        self.summary = "Anthony McGill, clarinet; " \
+                       "Milena Pajaro-van de Stadt, viola; " \
+                       "Gloria Chien, piano - " \
+                       "Philadelphia Chamber Music Society"
+
+    def test_get_event(self):
+        url = "https://www.pcmsconcerts.org/concerts/" \
+              "mcgill-pajaro-van-de-stadt-chien/"
+        res_event = self.scraper.get_event(url)
+
+        self.assertEqual(res_event["summary"], self.summary)
+        self.assertEqual(res_event["start"],
+                         dict(dateTime=self.startTime.isoformat(),
+                              timeZone=self.tz.zone))
